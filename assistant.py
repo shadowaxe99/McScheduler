cfeb3b482104f72378949d01953722d874fc4c86
Your Name <youremail@example.com>
2023-07-08 05:34:01 -0400

    Added much more functionality to the project

--- assistant.py
+++ assistant.py
@@ -26,5 +26,11 @@ class Assistant:
         elif task['type'] == 'email_to_speech':
             email = self.utils.parse_email(task['email'])
             return self.voice_support.text_to_speech(email['body'])
+        elif task['type'] == 'email_and_call':
+            email = self.utils.parse_email(task['email'])
+            call = self.utils.parse_call(task['call'])
+            email_response = self.email_support.send_email(email['to'], email['subject'], email['body'])
+            call_response = self.call_support.make_call(call['number'], call['message'])
+            return email_response + '\n' + call_response
         else:
             return 'Unknown task type'
