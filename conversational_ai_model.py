3b354b2c8c8731c19fe8539774268c2a3e600f12
Your Name <youremail@example.com>
2023-07-08 11:38:06 -0400

    Made generate_response method more verbose

--- conversational_ai_model.py
+++ conversational_ai_model.py
@@ -5,5 +5,7 @@ class ConversationalAIModel:
     def generate_response(self, user_input):
         import openai
         openai.api_key = 'your-real-api-key'
+        print('Generating response for user input:', user_input)
         response = openai.Completion.create(engine='text-davinci-002', prompt=user_input, max_tokens=150)
+        print('Generated response:', response.choices[0].text.strip())
         return response.choices[0].text.strip()
