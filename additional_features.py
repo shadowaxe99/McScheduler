92798fbc34789196974e7e7075cfd66930c4f544
Your Name <youremail@example.com>
2023-07-08 07:06:02 -0400

    Add additional features

---
+++ additional_features.py
@@ -0,0 +1,10 @@
+# This file will contain additional features for the McScheduler project.
+
+# Import necessary libraries
+import flask
+
+# Define additional routes or functions here
+
+@app.route('/additional')
+def additional():
+    return 'This is an additional feature.'
