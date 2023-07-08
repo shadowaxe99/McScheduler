cfa7aa8884aff7d28dd745b9ad91d1542b08262f
Your Name <youremail@example.com>
2023-07-08 05:29:54 -0400

    Added more functionality to the project

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ from email_handling_support import EmailHandlingSupport
 from automated_calls_support import AutomatedCallsSupport
 from assistant import Assistant
 from agent import Agent
-from voice import VoiceSupport
 
 def main():
     ai_model = ConversationalAIModel('gpt-3.5-turbo')
     ai_support = ConversationalAISupport(ai_model)
     email_support = EmailHandlingSupport('your_email@gmail.com', 'your_password')  # Replace with your email and password
     call_support = AutomatedCallsSupport('your_account_sid', 'your_auth_token')  # Replace with your Twilio account SID and auth token
-    voice_support = VoiceSupport()
     assistant = Assistant(ai_support, email_support, call_support)
     agent = Agent(assistant)
 
     while True:
-        user_input = voice_support.speech_to_text()
+        user_input = input('> ')
         task = {'type': 'conversation', 'input': user_input}
         response = agent.perform_task(task)
-        voice_support.text_to_speech(response)
+        print(response)
 
 if __name__ == '__main__':
     main()
