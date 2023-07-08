077525ded2c2c652f1cf565fc30e4d4e694896e8
Your Name <youremail@example.com>
2023-07-08 05:29:48 -0400

    Added more functionality to the project

--- assistant.py
+++ assistant.py
@@ -1,15 +1,25 @@
+from utils import Utils
+from voice import VoiceSupport
+
 class Assistant:
     def __init__(self, ai_support, email_support, call_support):
         self.ai_support = ai_support
         self.email_support = email_support
         self.call_support = call_support
+        self.utils = Utils()
+        self.voice_support = VoiceSupport()
 
     def handle_task(self, task):
         if task['type'] == 'conversation':
             return self.ai_support.handle_user_input(task['input'])
         elif task['type'] == 'email':
-            return self.email_support.send_email(task['to'], task['subject'], task['body'])
+            email = self.utils.parse_email(task['email'])
+            return self.email_support.send_email(email['to'], email['subject'], email['body'])
         elif task['type'] == 'call':
-            return self.call_support.make_call(task['number'], task['message'])
+            call = self.utils.parse_call(task['call'])
+            return self.call_support.make_call(call['number'], call['message'])
+        elif task['type'] == 'voice':
+            text = self.voice_support.speech_to_text()
+            return self.ai_support.handle_user_input(text)
         else:
             return 'Unknown task type'
--- voice.py
+++ voice.py
@@ -1,8 +1,22 @@
-class VoiceSupport:
-    def text_to_speech(self, text):
-        # TODO: Implement this method to convert text to speech
-        pass
+import pyttsx3
+import speech_recognition as sr
 
-    def speech_to_text(self, audio):
-        # TODO: Implement this method to convert speech to text
-        pass
+
+class VoiceSupport:
+    def __init__(self):
+        self.engine = pyttsx3.init()
+        self.recognizer = sr.Recognizer()
+
+    def text_to_speech(self, text):
+        self.engine.say(text)
+        self.engine.runAndWait()
+
+    def speech_to_text(self):
+        with sr.Microphone() as source:
+            audio = self.recognizer.listen(source)
+            try:
+                return self.recognizer.recognize_google(audio)
+            except sr.UnknownValueError:
+                return 'Could not understand audio'
+            except sr.RequestError as e:
+                return 'Could not request results; {0}'.format(e)
